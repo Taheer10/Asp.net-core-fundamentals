89d16536371582a6195906278783e27dc56f2814
Tahir <Tahirhussain32145@gmail.com>
2023-09-18 10:30:04 +0545

    View Model

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -22,9 +23,13 @@ namespace EmployeeManagement.Controllers
         public ViewResult Details()
         {
 
-            Employee model = _employeeRepository.GetEmployee(1);
-            ViewBag.PageTitle = "Employee Details";
-            return View(model);
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel
+            {
+                Employee = _employeeRepository.GetEmployee(1),
+                PageTitle = "Employee Details"
+            };
+       
+            return View(homeDetailsViewModel);
         }
 
         public IActionResult Privacy()
